732dd9545ccb34d34a4bcdb1161ab830e7e56bb5
JaYeong <kgy4529@gmail.com>
2022-06-13 16:24:34 +0900

    RestSampleApplication 수정

--- src/main/java/com/example/demo/RestSampleApplication.java
+++ src/main/java/com/example/demo/RestSampleApplication.java
@@ -1,13 +1,50 @@
 package com.example.demo;
 
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
+@MapperScan(value= {"com.example.demo.recipe.mapper"})
+//@ComponentScan(basePackages = "com.example.demo.recipe.service")
 public class RestSampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestSampleApplication.class, args);
-	}
-
+   public static void main(String[] args) {
+      SpringApplication.run(RestSampleApplication.class, args);
+   }
+   
+   //Mapper  인터페이스 인식실패 방지
+   @Bean
+   public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+       SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+       sessionFactory.setDataSource(dataSource);
+       
+       Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath*:mapper/*Mapper.xml");
+       
+       sessionFactory.setMapperLocations(res);
+       
+       return sessionFactory.getObject();
+   }
+   
+    @Bean
+    public SqlSessionTemplate sqlSession(SqlSessionFactory sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+   
+   @Bean
+   public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+       HiddenHttpMethodFilter filter = new HiddenHttpMethodFilter();
+       return filter;
+   }
 }
